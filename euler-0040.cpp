a69bf3e0b2b4afcdb99f2464926c1c5aa693a4fc
Kousigan <72304425+Kousigan1@users.noreply.github.com>
2021-03-22 17:48:46 +0530

    Update euler-0040.cpp

--- euler-0040.cpp
+++ euler-0040.cpp
@@ -1,141 +1,98 @@
-// ////////////////////////////////////////////////////////
-// # Title
-// Champernowne's constant
-//
-// # URL
-// https://projecteuler.net/problem=40
-// http://euler.stephan-brumme.com/40/
-//
-// # Problem
-// An irrational decimal fraction is created by concatenating the positive integers:
-//
-// `0.12345678910\red{1}112131415161718192021...`
-//
-// It can be seen that the 12th digit of the fractional part is 1.
-// If `d_n` represents the nth digit of the fractional part, find the value of the following expression.
-//
-// `d_1 * d_10 * d_100 * d_1000 * d_10000 * d_100000 * d_1000000`
-//
-// # Solved by
-// Stephan Brumme
-// February 2017
-//
-// # Algorithm
-// The original problem can be solved in a trivial way:
-// - a ''for''-loop appends numbers to a long string until that string contains enough digits
-// - read relevant digits, convert them from ASCII to integers and multiply them
-//
-// The Hackerrank problem asks for digits at positions up to `2^18` which cannot be done the brute force way
-// because we would be running out of memory (and CPU time).
-//
-// My function ''getDigit'' finds a digit without building such a long string.
-// It is based on the following observation:
-// - there are 9 numbers with one digit (1 to 9)
-// - there are 90 numbers with one digit (10 to 99)
-// - there are 900 numbers with one digit (100 to 999)
-// - ... and so on
-//
-// The first part of ''getDigit'' figures out how many digits the number has which is pointed to by the parameter ''pos''.
-// - the first 9 numbers are represented by 1*9 digits in Champernowne's constant
-// - the next 90 numbers are represented by 2*90=180 digits
-// - the next 900 numbers are represented by 3*900=2700 digits
-// - ... and so on: ''range'' will be 9, 90, 900, ... and ''digits'' will be 1, 2, 3, ...
-//
-// The variable ''skip'' will contain 9, 9+2*90 = 189, 9+2*90+3*900 = 2890 until the next step would exceed ''pos''.
-// Now that the function knows how many digit the number (pointed to by ''pos'') has, ''getDigit'' process its digits.
-//
-// To do so, it moves ''first'' closer to ''pos'' by repeated adding ''range''.
-// Whenever ''range'' becomes too large, the next (smaller) digit has to be adjusted until we have the final value of ''first''.
-// That number is converted to a string and the desired digit returned.
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define f(a,b,c)                for(int a=b;a<c;a++)
+#define s(x)                    scanf("%d",&x);
+#define sl(x)                   scanf("%lld",&x);
+#define p(x)                    printf("%d\n",x);
+#define p2(x,y)                 printf("%d %d\n",x,y);
+#define pl(x)                   printf("%lld\n",x);
+#define pl2(x,y)                printf("%lld %lld\n",x,y);
+#define p1d(a,n)                for(int ix=0;ix<n;ix++) printf("%d ",a[ix]); printf("\n");
+#define p2d(a,n,m)              for(int ix=0;ix<n;ix++){ for(int jx=0;jx<m;jx++) printf("%d ",a[ix][jx]); printf("\n");}
+void input(){
+    #ifdef Megamind
+            #define DEBUG
+            #define TRACE
+            freopen("inp.txt","r",stdin);
+            //freopen("out1.txt","w",stdout);
+    #endif
+}
+#ifdef TRACE
+    #define trace(x)                       cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<endl;
+    #define trace2(x,y)                    cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<endl;
+    #define trace3(x,y,z)                  cerr<<__FUNCTION__<<":"<<__LINE__<<": "#x" = "<<x<<" | "#y" = "<<y<<" | "#z" = "<<z<<endl;
+#else
+    #define trace(x)
+    #define trace2(x,y)
+    #define trace3(x,y,z) 
+#endif
 
-#include <string>
-#include <iostream>
-
-// return the digit at position "pos"
-// first digit after then decimal dot has pos = 1 (not zero !)
-unsigned int getDigit(unsigned long long pos)
-{
-  // assume pos has one digit
-  unsigned int       digits = 1;
-  // then there are 9 other numbers
-  unsigned long long range  = 9;
-  // the smallest of them is 1
-  unsigned long long first  = 1;
-
-  // there are    9 numbers with 1 digit
-  // there are   90 numbers with 2 digits
-  // there are  900 numbers with 3 digits
-  // there are 9000 numbers with 4 digits
-  // ...
-  // let's figure out the number of digits
-
-  // skip numbers with too few digits
-  unsigned long long skip = 0;
-  while (skip + digits*range < pos)
-  {
-    skip += digits*range;
-    // digits = 2 => range = 90 and
-    // digits = 3 => range = 900
-    // digits = 4 => range = 9000, etc.
-    digits++;
-    range *= 10;
-    first *= 10;
-  }
-
-  // now that we know the number of digits
-  // adjust "first" and "skip" such that the left-most/highest digit of pos and skip are identical
-  // then continue with the next digit
-  while (range > 9)
-  {
-    // could be replace by some modular arithmetic, but I'm too lazy for tough thinking ;-)
-    while (skip + digits*range < pos)
-    {
-      skip  += digits*range;
-      first += range;
+inline ll power(ll a, ll b, ll m) {
+    ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a % m;
+        a = (a * a)% m;
+        b >>= 1;
     }
+    return r;
+}
+inline ll power(ll a, ll b) {
+     ll r = 1;
+    while(b) {
+        if(b & 1) r = r * a ;
+        a = a * a;
+        b >>= 1;
+    }
+    return r;
+}
+ 
+/*........................................................END OF TEMPLATES.......................................................................*/
 
-    // next lower digit
-    range /= 10;
-  }
+ll digit[20],sum[20];
 
-  // right-most digit (basically same inner loop as above when range == 1)
-  while (skip + digits < pos)
-  {
-    first++;
-    skip += digits;
-  }
 
-  // skip all "skippable" digits
-  pos -= skip;
-  // strings are zero-based whereas input is one-based
-  pos--;
-
-  // create a string version of our number
-  auto s = std::to_string(first);
-  // extract digit and convert from ASCII to an integer
-  return s[pos] - '0';
+ll find(ll num){
+	int d=1;
+	while(sum[d]<num) d++;
+	
+	num-=sum[d-1];
+	
+	ll  number = power(10,d-1);
+	ll counter = (num-1)/d,extract = num%d;
+	if (!extract) extract = d;
+	number += counter;
+	
+	while(d != extract){
+		number/=10;
+		d--;
+	}
+	
+	return number%10;
 }
 
-int main()
-{
-  unsigned int tests;
-  std::cin >> tests;
-  while (tests--)
-  {
-    unsigned int product = 1;
-    // read 7 positions
-    for (unsigned int i = 0; i < 7; i++)
-    {
-      unsigned long long pos;
-      std::cin >> pos;
-
-      // multiply all digits
-      product *= getDigit(pos);
-    }
-
-    // print result
-    std::cout << product << std::endl;
-  }
-
-  return 0;
+int main(){
+	input();
+	ll temp = 9,ans;
+	sum[1]=9;
+	f(i,2,18){
+		temp*=10;
+		sum[i] = sum[i-1] + i*temp;
+	}
+	
+	int t;
+	s(t);
+	
+	while(t--){
+		ans = 1;
+		f(i,0,7) {
+			sl(temp);	
+			ans*=find(temp);
+		}
+		pl(ans);
+	}
+	
+	#ifdef Megamind
+		cout << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC * 1000 << " ms." << endl;
+    #endif
 }
